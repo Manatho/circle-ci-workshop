1c8104c1dd6a090c164a2fd9e507e2f2eac15fe6
Mathies-h <mathies-h@hotmail.com>
2019-08-14 10:19:09 +0200

    Elaborated item is never negative

--- src/test/java/net/praqma/codeacademy/gildedrose/GildedRoseTest.java
+++ src/test/java/net/praqma/codeacademy/gildedrose/GildedRoseTest.java
@@ -55,14 +55,14 @@ public class GildedRoseTest {
     @Test
     public void item_quality_is_never_negative() {
         Item[] items = new Item[] {
-            new Item("+5 Dexterity Vest", 10, 0), //
-            new Item("Aged Brie", 2, 0), //
+            new Item("+5 Dexterity Vest", 10, 2), //
+            new Item("Aged Brie", 2, 3), //
                 new Item("Elixir of the Mongoose", 5, 1), //
                 new Item("Sulfuras, Hand of Ragnaros", 0, 2), //
                 new Item("Sulfuras, Hand of Ragnaros", -1, 3),
                 new Item("Backstage passes to a TAFKAL80ETC concert", 15, 0),
                 new Item("Backstage passes to a TAFKAL80ETC concert", 10, 0),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 5, 0),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 5, 2),
                 // this conjured item does not work properly yet
                 new Item("Conjured Mana Cake", 3, 0) };
             GildedRose app = new GildedRose(items);
